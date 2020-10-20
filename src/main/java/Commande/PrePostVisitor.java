e93e38067d24b25233d99d92c8dfc353cacb507d
RAMIN <r.erwyn@gmail.com>
2020-10-20 17:02:46 +0200

    v1

--- src/main/java/Commande/PrePostVisitor.java
+++ src/main/java/Commande/PrePostVisitor.java
@@ -3,11 +3,11 @@ import Visitors.*;
 public interface PrePostVisitor extends AbstactVisitor{
     void preVisit(GroupeClient g);
     void preVisit(Client c);
-    void preVisit(Commande o);
+    void preVisit(Commande commande);
     void preVisit(Ligne l);
     void postVisit(GroupeClient g);
     void postVisit(Client c);
-    void postVisit(Commande o);
+    void postVisit(Commande commande);
     void postVisit(Ligne l);
 
 }
