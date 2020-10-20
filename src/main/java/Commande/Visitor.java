a6e9ef8ddfbc89847ea8cf3a6d8e0d1b141b70eb
RAMIN <r.erwyn@gmail.com>
2020-10-20 17:03:20 +0200

    v1.0

--- src/main/java/Commande/Visitor.java
+++ src/main/java/Commande/Visitor.java
@@ -3,6 +3,6 @@ import Visitors.*;
 public interface Visitor extends AbstactVisitor{
     void visit(GroupeClient g);
     void visit(Client c);
-    void visit(Commande o);
+    void visit(Commande commande);
     void visit(Ligne l);
 }
